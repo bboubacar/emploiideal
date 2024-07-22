f51a83a4c7d7f0b5898529967018ca51c876d2f4
bboubacar <bboubacarfr@gmail.com>
2024-07-22 19:05:09 +0200

    deployment test

--- src/utilities/constantes.jsx
+++ src/utilities/constantes.jsx
@@ -3,7 +3,7 @@ import { LiaFileContractSolid } from "react-icons/lia";
 import { FaCity } from "react-icons/fa";
 
 export const menuPaths = {
-    home: "/emploiideal",
+    home: "/",
     offres: "/offres",
     contact: "/contact",
     connexionCandidat: "/candidat/connexion",
