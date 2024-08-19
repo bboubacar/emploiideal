7ad8b5e4a29a92feec650f8bf91eb293906948a3
bboubacar <bboubacarfr@gmail.com>
2024-08-19 16:45:46 +0200

    modification du text description

--- src/hooks/usePageTitle.jsx
+++ src/hooks/usePageTitle.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 const usePageTitle = (pageTitle) => {
     useEffect(() => {
-        const baseTitle = "Emploi Idéal pour une meilleure carrière";
+        const baseTitle = "Emploi Idéal : Trouvez votre job parfait";
         document.title = `${pageTitle} - ${baseTitle}`;
     }, [pageTitle]);
 };
